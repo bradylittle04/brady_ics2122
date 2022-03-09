0d327f960a7b7d55e5371521a9b33763fd7b8e81
bradylittle04 <99288078+bradylittle04@users.noreply.github.com>
2022-03-09 14:36:17 -0500

    Update BradyP1.py

--- BradyP1.py
+++ BradyP1.py
@@ -1,36 +1,36 @@
-AllMovies = []
-Movie = []
-MovieName = input('Input Movie Name ')
-MovieLength = input('Input Movie Length ')
-MovieYear = int(input('Input Movie Year '))
-MovieDirector = input('Input Movie Director ')
-MovieCompany = input('Input Movie Company ')
-MovieDescription = input('InputMovie Description')
-MovieProduction = MovieYear -2 
-Movie.append(MovieName)
-Movie.append(MovieLength)
-Movie.append(MovieYear)
-Movie.append(MovieDirector)
-Movie.append(MovieCompany)
-Movie.append(MovieDescription)
-Movie.append(MovieProduction)
-AllMovies.append(Movie)
-print(AllMovies)
+AllSongs = []
+Song = []
+SongName = input('Input Song Name ')
+SongLength = input('Input Song Length ')
+SongYear = int(input('Input Song Year '))
+SongWriter = input('Input Song Writer ')
+SongProducer = input('Input Song Producer ')
+SongMeaning = input('Input Song Meaning ')
+SongProduction = SongYear -2 
+Song.append(SongName)
+Song.append(SongLength)
+Song.append(SongYear)
+Song.append(SongWriter)
+Song.append(SongProducer)
+Song.append(SongMeaning)
+Song.append(SongProduction)
+AllSongs.append(Song)
+print(AllSongs)
 
 command = input('enter command ')
-if command == 'Movie':
-    print(f'{MovieName}')
+if command == 'Song':
+    print(f'{SongName}')
 elif command == 'Length':
-    print(f'{MovieLength}')
+    print(f'{SongLength}')
 elif command == 'Year':
-    print(f'{MovieYear}')
-elif command == 'Director': 
-    print(f'{MovieDirector}')
-elif command == 'Company':
-    print(f'{MovieCompany}')
-elif command == 'Description':
-    print(f'{MovieDescription}')
+    print(f'{SongYear}')
+elif command == 'Writer': 
+    print(f'{SongWriter}')
+elif command == 'Producer':
+    print(f'{SongProducer}')
+elif command == 'Meaning':
+    print(f'{SongMeaning}')
 elif command == "Production":
-    print(f'{MovieProduction}')
+    print(f'{SongProduction}')
 else:
     print('Invalid Command')
